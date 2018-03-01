581fad48e69050773afdfafd45c700ba053a0351
John Firth <johfirth@gmail.com>
2018-02-28 19:31:59 -0500

    fix and put in loop

--- main.js
+++ main.js
@@ -8,17 +8,27 @@ for (var i = 0; i < word.length; i++) {
 }
 
 var currentResultEl = document.getElementById('current-result');
-var guessesDisplayEl = document.getElementById('previous-guesses');
+var guessesDisplayEl = document.getElementById('guessed');
 
 
 currentResultEl.innerText = dashes.join(' ');
 
 
-document.onkeyup = function (event) {
+document.onkeyup = function(event) {
     var key = event.key
+
+    // console.log(key)
    
    
     alreadyGuessed.push(key);
-    guessesDisplayEl.innertext = alreadyGuessed.join('  ')
+    guessesDisplayEl.innerText = alreadyGuessed.join('  ')
+
+    for (var i = 0; i < word.length; i++){
+
+        if (event.key === i)
+        
+    }
+
+
 
 }
