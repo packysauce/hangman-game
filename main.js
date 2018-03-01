a4c57234513f62f8659a388f275f85536026ed9e
John Firth <johfirth@gmail.com>
2018-02-28 23:49:26 -0500

    fix small details

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ var words = [
 var selectedWord = words[Math.floor(Math.random()*words.length)];
 var dashes = [];
 var alreadyGuessed = [];
-var wordArray = selectedWord.split("");
+var letterArray = selectedWord.split("");
 
 
-for (var i = 0; i < selectedWord.length; i++) {
+for (var i = 0; i < letterArray.length; i++) {
     dashes.push('_')
 }
 
@@ -40,15 +40,18 @@ document.onkeyup = function(event) {
     alreadyGuessed.push(key);
     guessesEl.innerText = alreadyGuessed.join('  ')
 
-    for (var j = 0; j < wordArray.length; j++){
-        var letters = wordArray[j];
+    for (var j = 0; j < letterArray.length; j++){
+        var guessLetter = letterArray[j];
 
 
-        if (letters === key){
-
-            console.log ("this is a good guess")
+        if (guessLetter === key){
             
-        } else { console.log ("this is not correct")}
+            console.log("this is correct")
+            
+
+            
+        } else { 
+            console.log ("this is not correct")}
         
     }
 
