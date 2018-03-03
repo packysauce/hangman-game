ec3eccea222edb9a0521c5e013a2d728a384bc9b
John Firth <johfirth@gmail.com>
2018-03-03 16:28:11 -0500

    alerting that you won

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var words = ["duck","fish","cat","dog","horse","donkey","pig","cow","goat","chic
 var selectedWord = words[Math.floor(Math.random() * words.length)];
 var dashes = [];
 var alreadyGuessed = [];
+var correctGuesses = [];
 var splitWord = selectedWord.split('');
 var currentResultEl = document.getElementById('current-result');
 var guessesEl = document.getElementById('guessed');
@@ -23,41 +24,44 @@ for (var i = 0; i < splitWord.length; i++) {
 }
 
 for (var i = 0; i < dashes.length; i++){
-    currentResultEl.appendChild(dashes[i])
+    currentResultEl.appendChild(dashes[i]);
 }
+
 
 document.onkeyup = function(event) {
     var key = event.key.toLowerCase();
 
-    console.log('keypress')
-
     if (okLetters.indexOf(key) === -1) {
         return;
-
-        console.log('good key')
     }
 
     if (alreadyGuessed.indexOf(key) !== -1){
         return;
-        console.log('already guessed')
     }
 
-    if (alreadyGuessed.length === 5){
-
+    if(splitWord.indexOf(key) !== -1){
+        correctGuesses.push(key);
     }
+
+    if (correctGuesses.length === dashes.length){
+        alert("You Won! The word was: " + selectedWord)
+    }
+    
+
 
     alreadyGuessed.push(key);
     guessesEl.innerText = alreadyGuessed.join(' ');
     
     var keyClass = classFor(key);
-
+    var blanks = document.getElementsByClassName(keyClass)
     for (var i = 0; i < blanks.length; i++) {
         blanks[i].innerHTML = key;
+
     }
-            
-
-
-
-
+    
 
 }
+
+
+
+
